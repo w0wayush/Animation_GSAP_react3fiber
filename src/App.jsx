7dd93ea5563fc4f770d758860e9ac2034261cc5f
Ayush Pandey <ayupandey82@gmail.com>
2024-11-17 14:17:45 +0530

    Updated few things

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Canvas } from '@react-three/fiber';
+import Experience from './components/Experience';
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Canvas
+      camera={{
+        fov: 64,
+        position: [2.3, 1.5, 2.3],
+      }}
+    >
+      <Experience />
+    </Canvas>
+  );
 }
